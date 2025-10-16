4df8cfb3994a87150ee91c3bca2e58e34d064854
elroi773 <s2457@e-mirim.hs.kr>
2025-10-16 19:01:39 +0900

    라우팅 문제 해결 / 면접 질문 페이지, 로그인 화면 구현 단계

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,9 @@
 <template>
-  <ShaderDemo />
+  <div id="app">
+    <router-view />
+  </div>
 </template>
 
-<script>
-import ShaderDemo from "./components/ShaderDemo.vue";
-
-export default {
-  components: { ShaderDemo },
-};
+<script setup>
+// App.vue는 단순히 route가 바뀌면 해당 컴포넌트를 렌더링함
 </script>
