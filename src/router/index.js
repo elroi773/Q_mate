348f81be830e8686b5e94a407727e65662cb18e3
elroi773 <s2457@e-mirim.hs.kr>
2025-10-30 19:22:27 +0900

    인터뷰 화면 구현

--- src/router/index.js
+++ src/router/index.js
@@ -2,11 +2,13 @@ import { createRouter, createWebHistory } from 'vue-router'
 import ShaderDemo from '../components/ShaderDemo.vue'
 import QuestionReady from '../Question_ready.vue'
 import Login from '../Login.vue'
+import Interview from '../Interview.vue'
 
 const routes = [
   { path: '/', component: ShaderDemo }, // ✅ 기본 진입점
   { path: '/question-ready', component: QuestionReady },
   { path: '/login', component: Login },
+  { path: '/interview', component: Interview },
 ]
 
 const router = createRouter({
