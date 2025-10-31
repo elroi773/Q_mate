a2eea5dee07d64387e4cb2564d3fdfde8044e481
elroi773 <s2457@e-mirim.hs.kr>
2025-10-31 20:18:25 +0900

    css 분리, 결과 페이지 구현

--- src/router/index.js
+++ src/router/index.js
@@ -3,12 +3,14 @@ import ShaderDemo from '../components/ShaderDemo.vue'
 import QuestionReady from '../Question_ready.vue'
 import Login from '../Login.vue'
 import Interview from '../Interview.vue'
+import Result from '../Result.vue'
 
 const routes = [
   { path: '/', component: ShaderDemo }, // ✅ 기본 진입점
   { path: '/question-ready', component: QuestionReady },
   { path: '/login', component: Login },
   { path: '/interview', component: Interview },
+  { path: '/result', component: Result },
 ]
 
 const router = createRouter({
