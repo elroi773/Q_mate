bd43f1e5d14ccb012e0e7ff8a4a4b6a995c686c5
elroi773 <s2457@e-mirim.hs.kr>
2025-11-04 16:51:22 +0900

    회원가입 페이지 구현

--- src/router/index.js
+++ src/router/index.js
@@ -4,11 +4,13 @@ import QuestionReady from '../Question_ready.vue'
 import Login from '../Login.vue'
 import Interview from '../Interview.vue'
 import Result from '../Result.vue'
+import Join from "../Join.vue"
 
 const routes = [
   { path: '/', component: ShaderDemo }, // ✅ 기본 진입점
   { path: '/question-ready', component: QuestionReady },
   { path: '/login', component: Login },
+  { path: '/Join', component: Join },
   { path: '/interview', component: Interview },
   { path: '/result', component: Result },
 ]
