d2b43b05278071164edef4e272e9200db3f39ce7
elroi773 <s2457@e-mirim.hs.kr>
2025-11-04 16:51:14 +0900

    회원가입 페이지 구현

---
+++ src/Join.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="login-container">
+      <h2>회원가입</h2>
+      <input
+        v-model="id"
+        type="text"
+        placeholder="아이디"
+      />
+      <input
+        v-model="password"
+        type="password"
+        placeholder="비밀번호"
+      />
+      <button @click="login">로그인</button>
+      <p v-if="message">{{ message }}</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: "Join",
+    data() {
+      return {
+        id: "",
+        password: "",
+        message: "",
+      };
+    },
+    methods: {
+      
+    },
+  };
+  </script>
+  
+  
